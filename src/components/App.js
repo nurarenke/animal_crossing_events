d39e9defb6d26f142ae06ebdb3eb038e21f220d8
Nura Renke <nurarenke@Nuras-MacBook-Pro.local>
2020-07-22 09:59:53 -0700

    Add Axios Instance of Nookpedia

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,47 @@
 import React from "react";
 import Calendar from "./Calendar";
+import nookipedia from "../apis/nookipedia";
 
 class App extends React.Component {
+  state = { events: [] };
+
+  componentDidMount() {
+    const todaysDate = "may 2";
+    this.findTodaysEvents();
+    console.log("mounted");
+  }
+
+  findTodaysEvents = async () => {
+    try {
+      const res = await nookipedia.get("/today/");
+      console.log("Success!");
+      console.log(res.status);
+      console.log(res.data);
+    } catch (e) {
+      if (e.response) {
+        console.log("request made");
+        console.log(e.response.status);
+        console.log(e.response.data);
+        console.log(e.response.headers);
+      } else if (e.request) {
+        console.log(e.request);
+      } else {
+        // Something happened in setting up the request and triggered an Error
+        console.log("Error", e.message);
+      }
+      console.log(e);
+    }
+
+    // this.setState({
+    //   events: res.data.events.message,
+    // });
+  };
+
   render() {
     return (
       <div className="ui container">
         <Calendar />
+        <div>events:{this.state.events}</div>
       </div>
     );
   }
