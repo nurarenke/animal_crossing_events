3e75b2162870f66f8bf9be40313cacba52726b02
Nura Renke <nurarenke@nuras-mbp.lan>
2021-04-06 19:01:24 -0500

    Add CSS to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
+import "./Header.css";
 import React from "react";
 
 const Header = () => {
   return (
-    <div>
-      <img className="ui middle aligned tiny image" src="https://dodo.ac/np/images/7/78/Biskit_NH.png"></img>
-      <h3 className="ui block header">
-        Block Header
-      </h3>
+    <div className="ui padded segment title-row">
+      <img className="ui tiny image item" src="https://dodo.ac/np/images/7/78/Biskit_NH.png" alt="Biskit"></img>
+      <h1 className="ui green header item" id="header-title">
+          Animal Crossing Events
+      </h1>
+      <img className="ui tiny image item" src="https://dodo.ac/np/images/9/91/Fauna_NH.png" alt="Fauna"></img>
     </div>
   );
 };
